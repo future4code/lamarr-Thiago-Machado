d68f0f23300503f0d9b25912b0b213ab94bfa2d9
Thiago Luca Faustino Machado <thiago123luca@gmail.com>
2022-10-14 18:20:41 -0300

    exercicio-typescript

---
+++ modulo5/intro-typescript/Execicio2/src/index.ts
@@ -0,0 +1,11 @@
+const cor1: string= process.argv[2]
+const cor2: string= process.argv[3]
+const cor3: string= process.argv[4]
+
+
+
+function imprimeTresCoresFavoritas(cor1: string, cor2: string, cor3: string ):void{
+  console.log([cor1, cor2, cor3])
+}
+
+imprimeTresCoresFavoritas(cor1, cor2, cor3)
