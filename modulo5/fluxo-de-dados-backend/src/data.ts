47d306427abdd1c4090a171fef63ecece4f907e6
Thiago Luca Faustino Machado <thiago123luca@gmail.com>
2022-11-01 21:38:05 -0300

    fluxo-de-dado-backend

---
+++ modulo5/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,18 @@
+import { Produtos } from "./type"
+export const data: Produtos[] = [
+    {
+        id: Date.now().toString(),
+        name: "maça",
+        price: 2.50
+    },
+    {
+        id: Date.now().toString(),
+        name: "pera",
+        price: 3.00
+    },
+    {
+        id: Date.now().toString(),
+        name: "banana",
+        price: 1.30
+    },
+]
